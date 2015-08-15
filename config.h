8ebdc17f0243d317e7120e1e9a51a701aa4a38c8
Vincent ANTOINE <contact.vantoine@gmail.com>
2015-08-15 22:14:55 +0200

    Update config.h
    Small update for better performance on wifi shield

--- config.h
+++ config.h
@@ -6,7 +6,8 @@ String sNumber ="C01000000001";
 static const char ssid[] = "orion"; 
 static const char pass[] = "We85R30000";
 char server[] = "www.chicon.fr";
-static const String WEBSERVICE_URL = "/chicon/webServices/hdwWS.php";
+//static const String WEBSERVICE_URL = "/chicon/webServices/hdwWS.php";
+static const String WEBSERVICE_URL = "/h.php";
 static char HTTP_REQUEST[] = " HTTP/1.1\r\nHost:www.chicon.fr\r\n\r\n";
 
 #endif
